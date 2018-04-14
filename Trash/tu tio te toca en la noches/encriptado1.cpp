89d63393bb172d5441853c53e3941d5168827557
Patrick Márquez <dibujanteanime024@gmail.com>
2018-04-14 14:25:14 -0500

    nomesale;-;

---
+++ Trash/tu tio te toca en la noches/encriptado1.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+class Encriptado
+{
+private:
+	string alfabeto="abcdefghijklmnopqrtusvwxyz ";
+	string mensaje;
+	unsigned int clave;
+public:
+	Encriptado(string, unsigned int);
+	string encrip_mensa();
+	
+};
+
+Encriptado::Encriptado(string mensaje1, unsigned int c1)
+{
+	mensaje=mensaje1;
+	clave=c1;
+	
+}
+string Encriptado::encrip_mensa(){
+	
+	string mensaje_perm;
+	for(unsigned int i= 0; i<mensaje.size(); i++){
+		mensaje_perm+=alfabeto[alfabeto.size()-1-alfabeto.find(mensaje[i])]; ///concatenar append
+	}
+	cout <<mensaje_perm<<endl;
+	string mensaje_encriptado;
+	int cont1=0;
+	int a = 2*(clave-1);
+	while(cont1<mensaje_perm.size()-1){
+		mensaje_encriptado+=mensaje_perm[cont1];
+		cont1+=a;
+	}
+	mensaje_encriptado+=' ';
+	cont1 = 1;
+	///a = 
+	while(cont1<mensaje_perm.size()-1){
+		mensaje_encriptado+=mensaje_perm[cont1];
+		cont1+=2*(clave-2);
+	}
+	return mensaje_encriptado;
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	string mensj,str1;
+	cout <<"Ingrese el mensaje :";
+	getline(cin,str1); ///Sobrecarga de Getline en un string
+	cout <<str1<<endl;
+	unsigned int clave;
+	cout <<"Ingrese la clave : "; 
+	cin >> clave;
+	
+	Encriptado e(str1, clave);
+	mensj=e.encrip_mensa();
+	
+	cout <<mensj<<endl;
+	return 0;
+}
+
---
+++ Trash/tu tio te toca en la noches/encriptado2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include "Proceso.h"
+
+using namespace std;
+
+class Proceso
+{
+private:
+	string mensaje;
+	string alfabeto= "abcdefghijklmnopqrstuvwxyz";
+	unsigned int n_len;
+	unsigned int n;
+public:
+	Proceso(unsigned int, unsigned int, string);
+	string encriptacion();
+	void desencriptacion(string);
+	
+};
+Proceso::Proceso(unsigned int n_cuadrados, unsigned int n_len_cuadrado, string mensaje1)
+{
+	n=n_cuadrados;
+	n_len=n_len_cuadrado;
+	mensaje=mensaje1;
+}
+string Proceso::encriptacion()
+{
+	string mensaje_encriptado1;
+	unsigned int cont=0;
+	for(int i = 0; i<n_len; i++){
+		for(int j = 0; j<n_len; j++){
+			///mensaje_encriptado.insert(mensaje_encriptado.begin()+i+n_len-i, mensaje[j*n_len+n_len-i]);
+			mensaje_encriptado1.insert(mensaje_encriptado1.begin()+(j*n_len)+n_len-i, mensaje[cont]);
+			cont++;
+		}
+	}
+	cout << mensaje_encriptado1<<endl;
+	
+}
+void Proceso::desencriptacion(string)
+{
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	/*ifstream fichero("texto.txt");
+	ofstream fichero_temporal("textotemp.txt");
+	if(!fichero || !fichero_temporal){
+	cout << "Error al abrir el archivo!" << endl;
+}
+	string mensaje, contenido;
+	while(getline(fichero, contenido))
+	{
+	mensaje+= contenido;
+	///encriptacion();
+	///mensaje+= '\n'; saltos de linea
+}
+	fichero_temporal << mensaje << endl;
+	cout << mensaje <<endl;
+	fichero.close();
+	fichero_temporal.close();
+	remove("texto.txt");
+	rename("textotemp.txt", "texto.txt");*/
+	///Proceso p(4,5,"Hola como estas yo me llamo ");
+	///p.encriptacion();
+	/*string prueba = "Hola como estas yo me llamo nipples";
+	///prueba[1]='p';
+	cout <<prueba[1]<<" - "<<prueba[4]<<" - "<<prueba[9]<<endl;
+	string mensaje_encriptado1;
+	unsigned int cont=0, n_len=5;
+	for(int i = 0; i<n_len; i++){
+	for(int j = 0; j<n_len; j++){
+	///mensaje_encriptado.insert(mensaje_encriptado.begin()+i+n_len-i, mensaje[j*n_len+n_len-i]);
+	mensaje_encriptado1.insert(mensaje_encriptado1.begin()+(j*n_len)+n_len-i, prueba[cont]);
+	cont++;
+}
+}
+	cout << mensaje_encriptado1<<endl;
+	*/
+	return 0;
+}
+
